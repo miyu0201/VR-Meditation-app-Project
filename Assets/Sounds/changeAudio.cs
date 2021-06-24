63e7425efbca6649a8043f1402702fa1cf2f6a78
miyu0201 <y.minghui@yahoo.com>
2021-06-24 17:34:32 +0200

    first commit

---
+++ Assets/Sounds/changeAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class changeAudio : MonoBehaviour
+{
+    public AudioClip nightClip;
+    public AudioClip dayClip;
+    
+    public GameObject TimeOfDayManager;
+    public AC.TimeOfDaySystemFree.TimeOfDay timeOfDayScript;
+    public float dayInSeconds;
+
+    public float delayTime;
+
+
+    IEnumerator Start()
+    {
+        timeOfDayScript = TimeOfDayManager.GetComponent<AC.TimeOfDaySystemFree.TimeOfDay>();
+        dayInSeconds = timeOfDayScript.dayInSeconds;
+        delayTime = dayInSeconds / 2;
+
+        AudioSource audio = GetComponent<AudioSource>();
+
+        audio.Play();
+        yield return new WaitForSeconds(delayTime);
+     
+        audio.clip = nightClip;
+        audio.volume = 1f;
+        audio.Play();
+
+        yield return new WaitForSeconds(delayTime);
+        audio.clip = dayClip;
+        audio.Play();
+    }
+}
